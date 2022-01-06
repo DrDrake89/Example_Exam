3cc9ec98b5164ae44c27feb00ed8fe1817fe443d
DrDrake89 <79028763+DrDrake89@users.noreply.github.com>
2022-01-06 23:22:38 +0100

    Example

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -15,12 +15,17 @@ public class MyGdxGame extends ApplicationAdapter {
 	public static Texture sheet;
 	public static TextureRegion background,protagonista;
 	public static float totalScreenWidth,totalScreenHeight;
+	public float PositionX;
+	public float PositionY;
 
 	@Override
 	public void create () {
 
 		totalScreenWidth = Gdx.graphics.getWidth();
 		totalScreenHeight = Gdx.graphics.getHeight();
+
+		PositionX = totalScreenHeight/2;
+		PositionY = totalScreenWidth/2;
 
 		sheet = new Texture(Gdx.files.internal("Pictures.png"));
 		sheet.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
@@ -38,8 +43,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		float TotalSteps = 0;
-		float PositionX = totalScreenHeight/2;
-		float PositionY = totalScreenWidth/2;
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		//batch.draw(img, 0, 0);
@@ -47,14 +50,13 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.enableBlending();
 		batch.draw(protagonista,PositionY,PositionX);
-		while(PositionY<totalScreenWidth){
+
+		/*while(PositionY<totalScreenWidth){
 			for(TotalSteps = PositionY; TotalSteps<= totalScreenWidth; TotalSteps++){
 				System.out.println(TotalSteps);
+				PositionY = PositionY+TotalSteps;
 			}
-
-		}
-
-
+		}*/
 
 		batch.end();
 	}
